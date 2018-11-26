1350e2de02b231bb70a4a682d07db990b80f61a7
Rahul <nemillarahul@gmail.com>
2018-11-26 10:26:58 +0530

    python programme

---
+++ ex1.py
@@ -0,0 +1,8 @@
+num1 =10
+num2 =20
+print("Num1 + Num2 =", num1+num2)
+print("Num1 - Num2 =", num1-num2)
+print("Num1 * Num2 =", num1*num2)
+print("Num1 / Num2 =", num1/num2)
+num3 =num1+num2
+print ('num3=', num3)
